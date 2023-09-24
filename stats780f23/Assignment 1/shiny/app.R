7b4145c475103c0465e2e39f1a8a997c004a8f7c
PZVivian <35195917+PZVivian@users.noreply.github.com>
2023-09-24 14:41:13 -0400

    UPDATE: Various changes
    - Added more labels to the x-axis of the graphs
    - Added back the shiny folder
    - Added a line in A1.qmd that saves the clean data into the shiny folder so it can be used by the app (prevents having to clean the data again)
    - Minor comment changes to improve clarity

---
+++ stats780f23/Assignment 1/shiny/app.R
@@ -0,0 +1,63 @@
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(stringi)
+
+
+### DATA PRE-PROCESSING ###
+# Load cleaned disposal data
+load("disposalData.RData")
+
+# Dropdown options
+provinceOptions <- disposalData %>%
+  select(`Province`) %>% 
+  distinct(`Province`) %>%
+  pull()
+
+
+### APP UI ###
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Yearly Asbestos Waste by Province and Sector"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = "province",
+                        label = "Province",
+                        choices = provinceOptions
+            )
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("lineGraph")
+        )
+    )
+)
+
+
+### SERVER LOGIC ###
+server <- function(input, output) {
+  
+  output$lineGraph <- renderPlot({
+    
+    # Filter waste data by user's province selection
+    disposalData_line <- disposalData %>% 
+      filter(`Province` == input$province) %>% 
+      group_by(`Year`, `Sector Name`) %>% 
+      summarize("Quantity of Asbestos (Tonnes)" = sum(`Quantity (Tonnes)`))
+    
+    # Plot line graph showing waste quantity by year and sector for the selected province 
+    disposalData_line %>% 
+      ggplot(aes(x=`Year`, y=`Quantity of Asbestos (Tonnes)`, color=`Sector Name`)) +
+      geom_line() +
+      scale_x_continuous(breaks = round(seq(min(disposalData_line$`Year`), max(disposalData_line$`Year`), by = 4),1))
+  })
+  
+}
+
+
+### RUN APP ###
+shinyApp(ui = ui, server = server)
